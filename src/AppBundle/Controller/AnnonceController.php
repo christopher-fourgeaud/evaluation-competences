c6e8dacc051a363d8b198e5d56edb80f840cd62a
christopher-fourgeaud <christopher.fourgeaud@gmail.com>
2018-02-02 16:52:04 +0100

    Ajout de l'input de reccherche + requète

---
+++ src/AppBundle/Controller/AnnonceController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class AnnonceController extends Controller
+{
+    /**
+     * @Route("/liste")
+     */
+    public function listAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $annonces = $em->getRepository("AppBundle:Annonce")->findAll();
+
+        $form = $this->createFormBuilder()
+        ->add("recherche", SearchType::class)
+        // ->add("type", EntityType::class, array(
+        //         'class' => 'AppBundle:TypeAnnonce',
+        //     ))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            $data = $form->getData();
+            $parameter = $data["recherche"];
+
+            $query = $em->createQuery("select a from AppBundle\Entity\Annonce as a
+            where a.titre like :p ")
+                ->setParameter('p', '%' . $parameter . '%');
+            $annonces = $query->getResult();
+        }
+
+        return $this->render('/annonce/liste_annonces.html.twig', array(
+            'annonces' => $annonces,
+            'form' => $form->createView()
+        ));
+    }
+}
--- src/AppBundle/Repository/AnnonceRepository.php
+++ src/AppBundle/Repository/AnnonceRepository.php
@@ -10,4 +10,5 @@ namespace AppBundle\Repository;
  */
 class AnnonceRepository extends \Doctrine\ORM\EntityRepository
 {
+    
 }
